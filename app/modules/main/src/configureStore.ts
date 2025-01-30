281b9f6fd00c7d06197f0ba88d0a4985d11c7351
ABDULMALEK  AL-SHUGAA <33663456+AbdulmalekAlshugaa@users.noreply.github.com>
2025-01-31 04:02:41 +0800

    chore: clean up some unused imported file

--- app/modules/main/src/configureStore.ts
+++ app/modules/main/src/configureStore.ts
@@ -1,7 +1,4 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
-import authReducer from '../../auth/src/authReducer'
-
+import {  configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
