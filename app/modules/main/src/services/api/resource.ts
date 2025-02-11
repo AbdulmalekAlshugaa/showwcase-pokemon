0e7116f1872ed5e3e268be5f0c4bbdf1ddeac616
ABDULMALEK  AL-SHUGAA <33663456+AbdulmalekAlshugaa@users.noreply.github.com>
2025-02-11 15:33:47 +0800

    chore: fix api calls general calls

--- app/modules/main/src/services/api/resource.ts
+++ app/modules/main/src/services/api/resource.ts
@@ -20,39 +20,23 @@ export default class ResourceApi {
         return this.api.transformResponse(response);
     }
 
-    async getById<T = unknown>(id: string | number, params: {} | undefined, config?: AxiosRequestConfig) {
-        const response = await this.api.apisauce.get<BasicResponse<T>>(`${this.endpoint}/${id}`, params, config);
-        return this.api.transformResponse(response);
-    }
-
-    async getByParameters<T = unknown>(params: {} | undefined, config?: AxiosRequestConfig) {
-        const response = await this.api.apisauce.get<BasicResponse<T>>(`${this.endpoint}`, params, config);
-
-        return this.api.transformResponse(response);
-    }
-
     async post<T = unknown, U = unknown>(payload?: U, config?: AxiosRequestConfig) {
         const response = await this.api.apisauce.post<BasicResponse<T>>(`${this.endpoint}`, payload, config);
         return this.api.transformResponse(response);
     }
 
-    async postById<T = unknown, U = unknown>(id: string | number, payload: U, config?: AxiosRequestConfig) {
-        const response = await this.api.apisauce.post<BasicResponse<T>>(`${this.endpoint}/${id}`, payload, config);
-        return this.api.transformResponse(response);
-    }
-
-    async deleteById(id: string | number, config?: AxiosRequestConfig) {
-        const response = await this.api.apisauce.delete<BasicResponse>(`${this.endpoint}/${id}`, config);
-        return this.api.transformResponse(response);
-    }
-
-    async deleteByParameters<T = unknown, U = unknown>(payload: U, config?: AxiosRequestConfig) {
-        const response = await this.api.apisauce.delete<BasicResponse<T>>(this.endpoint, payload, config);
-        return this.api.transformResponse(response);
-    }
-
     async patch<T = unknown, U = unknown>(payload: U, config?: AxiosRequestConfig) {
         const response = await this.api.apisauce.patch<BasicResponse<T>>(this.endpoint, payload, config);
+        return this.api.transformResponse(response);
+    }
+
+    async put<T = unknown, U = unknown>(payload: U, config?: AxiosRequestConfig) {
+        const response = await this.api.apisauce.put<BasicResponse<T>>(this.endpoint, payload, config);
+        return this.api.transformResponse(response);
+    }
+
+    async delete<T = unknown>(config?: AxiosRequestConfig) {
+        const response = await this.api.apisauce.delete<BasicResponse<T>>(this.endpoint, config);
         return this.api.transformResponse(response);
     }
 }
